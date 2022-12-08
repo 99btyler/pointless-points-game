a80a06cc323ef807a4b45737329b7ae9ab6a4bc4
99btyler <99tylerberinger@gmail.com>
2022-12-08 15:42:57 -0500

    Added level increases

--- src/index.js
+++ src/index.js
@@ -39,6 +39,10 @@ class GameEventEmitter {
 
     }
 
+    clear() {
+        this.listeners = {};
+    }
+
 }
 
 class Player extends GameObject {
@@ -54,13 +58,13 @@ class Player extends GameObject {
             }
         });
         gameEventEmitter.register(events.KEYDOWN, () => {
-            if (this.y < (texture.height * TEXTURES_PER_COLUMN) - texture.height) {
+            if (this.y < (texture.height * textures_per_side) - texture.height) {
                 this.y += texture.height;
                 gameEventEmitter.emit(events.PLAYERMOVE);
             }
         });
         gameEventEmitter.register(events.KEYRIGHT, () => {
-            if (this.x < (texture.width * TEXTURES_PER_ROW) - texture.width) {
+            if (this.x < (texture.width * textures_per_side) - texture.width) {
                 this.x += texture.width;
                 gameEventEmitter.emit(events.PLAYERMOVE);
             }
@@ -73,7 +77,27 @@ class Player extends GameObject {
         });
 
         gameEventEmitter.register(events.PLAYERMOVE, () => {
+
             gameObjects.push(new Point(texturePoint, this.x, this.y));
+
+            if (gameObjects.length >= (textures_per_side * textures_per_side)+1) {
+
+                // settings update
+                if (textures_per_side >= max_textures_per_side) {
+                    textures_per_side = min_textures_per_side;
+                    startingX = 0;
+                    startingY = 0;
+                } else {
+                    textures_per_side += 2;
+                    startingX = this.x;
+                    startingY = this.y;
+                }
+
+                // Recreate the game objects
+                resetGame();
+
+            }
+
         })
 
     }
@@ -89,9 +113,6 @@ class Point extends GameObject {
 }
 
 
-const TEXTURES_PER_ROW = 6;
-const TEXTURES_PER_COLUMN = 6;
-
 const gameObjects = [];
 const gameEventEmitter = new GameEventEmitter();
 const events = {
@@ -104,6 +125,13 @@ const events = {
     PLAYERMOVE: "PLAYERMOVE"
 
 };
+
+// settings
+const min_textures_per_side = 3;
+const max_textures_per_side = 9;
+let textures_per_side = min_textures_per_side;
+let startingX = 0;
+let startingY = 0;
 
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
@@ -121,36 +149,56 @@ window.onload = async () => {
     initGame();
 
     // Start drawing
-    canvas.width = texturePlayer.width * TEXTURES_PER_ROW;
-    canvas.height = texturePlayer.height * TEXTURES_PER_COLUMN;
-    canvasContext.fillStyle = "#202020";
-
     window.requestAnimationFrame(drawGame);
 
 }
 
 function loadTexture(path) {
+
     return new Promise((resolve) => {
+
         const texture = new Image();
         texture.src = path;
+
         texture.onload = () => {
             resolve(texture);
         }
+
     });
+
 }
 
 function initGame() {
-    gameObjects.push(new Player(texturePlayer, 0, 0));
-    gameObjects.push(new Point(texturePoint, 0, 0));
+
+    // Set the screen size
+    canvas.width = texturePlayer.width * textures_per_side;
+    canvas.height = texturePlayer.height * textures_per_side;
+
+    gameObjects.push(new Player(texturePlayer, startingX, startingY));
+    gameObjects.push(new Point(texturePoint, startingX, startingY));
+
 }
 
 function drawGame() {
 
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    canvasContext.fillStyle = "#202020";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     gameObjects.forEach((gameObject) => gameObject.draw());
 
     window.requestAnimationFrame(drawGame);
+
+}
+
+function resetGame() {
+
+    while (gameObjects.length > 0) {
+        gameObjects.pop();
+    }
+    gameEventEmitter.clear();
+
+    initGame();
 
 }
 
